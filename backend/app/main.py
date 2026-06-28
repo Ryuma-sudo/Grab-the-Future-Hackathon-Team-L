73699e6f75f1e24799833423f1736b8b7ae12231
Ryuma-sudo <quangtruc6a1vx@gmail.com>
2026-06-28 11:06:37 +0700

    allow vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4028",
         "http://127.0.0.1:4028",
+        "https://grab-the-future-hackathon-team-l.vercel.app",
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
